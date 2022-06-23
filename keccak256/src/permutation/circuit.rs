fb5e8297e4ce8db3151b8cc987be85ad28c7bee2
ChihChengLiang <chihchengliang@gmail.com>
2022-06-24 00:16:31 +0100

    use hash map for lookup

--- keccak256/src/permutation/circuit.rs
+++ keccak256/src/permutation/circuit.rs
@@ -132,7 +132,7 @@ impl<F: Field> KeccakFConfig<F> {
         }
     }
 
-    pub fn load(&self, layouter: &mut impl Layouter<F>) -> Result<(), Error> {
+    pub fn load(&mut self, layouter: &mut impl Layouter<F>) -> Result<(), Error> {
         self.stackable.load(layouter)?;
         self.base13to9_config.load(layouter)?;
         self.from_b9_table.load(layouter)
@@ -334,7 +334,7 @@ mod tests {
 
             fn synthesize(
                 &self,
-                config: Self::Config,
+                mut config: Self::Config,
                 mut layouter: impl Layouter<F>,
             ) -> Result<(), Error> {
                 // Load the table
